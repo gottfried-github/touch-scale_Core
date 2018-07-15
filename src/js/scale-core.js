3a4f13b487590d10f262fd26e4d2f5d4179f750c
spti <wsa@localhost.localdomain>
2018-07-15 19:22:47 +0300

    add calculateTransform

--- src/js/scale-core.js
+++ src/js/scale-core.js
@@ -2,6 +2,43 @@ function ScaleCore() {
 
 }
 
+ScaleCore.prototype.calculateTransform = function(transforms, rects, containerDims, target) {
+
+  const transformsNew = {}
+  Object.assign(transformsNew, transforms)
+
+  // this.coordinates.rects = this.el.getBoundingClientRect();
+
+  // figure out the dimensions for the element to obtain after scaling:
+  var ratio = target / transforms.scaleX;
+
+  // we make projection of the future scaled element...
+  const transformsProjected = {
+    rects: {
+      left: (rects.left + rects.width / 2) - (transforms.originX * target),
+      top: (rects.top + rects.height / 2) - (transforms.originY * target),
+      width: rects.width * ratio,
+      height: rects.height * ratio
+    },
+    // if we'd work with changing origin, these values in transforms would be altered by scaleStart.
+    // in our case, however, these remain the same as they were left after the previous scaling of the element.
+    translateX: transforms.translateX,
+    translateY: transforms.translateY
+  }
+
+  // ... to see, if it would overflow the borders of the viewport, and, if so,
+  // adjust the translation to avoid that
+  var transformsBounded = this.encounterBounds(transformsProjected, containerDims);
+
+  // set the values that the matrix of the element should have
+  transformsNew.translateX = transformsBounded.translateX
+  transformsNew.translateY = transformsBounded.translateY
+  transformsNew.scaleX = target
+  transformsNew.scaleY = target
+
+  return transformsNew
+}
+
 ScaleCore.prototype.encounterBounds = function(coords, containerDims) {
 
   var array = [
