b974b35064e821a696f73f23a2db14314ade0534
spti <wsa@localhost.localdomain>
2018-07-16 16:39:14 +0300

    refactor: separate projecting into a separate function

--- src/js/scale-core.js
+++ src/js/scale-core.js
@@ -2,6 +2,92 @@ function ScaleCore() {
 
 }
 
+ScaleCore.prototype.calculateTransform = function(target, transforms, rects, parent) {
+
+  // project, what rects of el would look like after it's scaled
+  const rectsProjected = this.projectRects(target, transforms, rects)
+
+  // adjust the el's translation to fit it in it's container nicely
+  const transformsBounded = this.encounterBounds(transforms, rects, parent)
+
+  return transformsBounded
+}
+
+ScaleCore.prototype.projectRects = function(target, rects, transforms) {
+
+  // this.coordinates.rects = this.el.getBoundingClientRect();
+
+  // figure out the dimensions for the element to obtain after scaling:
+  var ratio = target / transforms.scaleX;
+
+  // we make projection of the future scaled element...
+  const rectsProjected = {
+    left: (rects.left + rects.width / 2) - (transforms.originX * target),
+    top: (rects.top + rects.height / 2) - (transforms.originY * target),
+    width: rects.width * ratio,
+    height: rects.height * ratio
+  }
+
+  return rectsProjected
+}
+
+ScaleCore.prototype.encounterBounds = function(transforms, rects, parent) {
+
+  var array = [
+    {
+      length: rects.width,
+      pos: rects.left,
+      translation: transforms.translateX,
+      parent: parent.width // parseInt(getViewportWidth())
+    },
+    {
+      length: rects.height,
+      pos: rects.top,
+      translation: transforms.translateY,
+      parent: parent.height // parseInt(getViewportHeight())
+    }
+  ]
+
+  for (var i = 0; i < array.length; i++) {
+    var temp = process(array[i]);
+    // console.log("tweakIt", temp)
+    array[i].newPos = (typeof(temp) === 'number') ? temp : array[i].translation;
+  }
+
+  const transformsNew = {}
+
+  // Object.assign is es6, but there's a polyfill, in case of anything:
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
+  Object.assign(transformsNew, transforms)
+
+  transformsNew.translateX = array[0].newPos;
+  transformsNew.translateY = array[1].newPos;
+
+  function process(axis) {
+
+    if (axis.length <= axis.parent) {
+      if (axis.pos > (axis.parent - axis.length)) {
+        return ( axis.translation - (axis.pos - (axis.parent - axis.length)) );
+      } else if (axis.pos < (0)) {
+        return ( axis.translation + Math.abs(axis.pos) + 2 );
+      }
+      return false;
+    } else if (axis.length > axis.parent) {
+      if (axis.pos > (0)) {
+        return ( axis.translation - axis.pos );
+      } else if (axis.pos < (axis.parent - axis.length)) {
+        return ( (axis.translation + ( Math.abs(axis.pos) - Math.abs(axis.parent - axis.length) )) );
+      }
+      return false;
+    }
+    return false;
+  }
+
+  return transformsNew;
+  //this.element.css( 'transform', 'matrix(' + coords.scaleX + ', 0, 0, ' + coords.scaleY +  ', ' + x.newPos + ', ' + y.newPos + ')' );
+
+}
+
 ScaleCore.prototype.calculateTransform = function(transforms, rects, containerDims, target) {
 
   const transformsNew = {}
@@ -39,63 +125,6 @@ ScaleCore.prototype.calculateTransform = function(transforms, rects, containerDi
   return transformsNew
 }
 
-ScaleCore.prototype.encounterBounds = function(coords, containerDims) {
-
-  var array = [
-    {
-      length: coords.rects.width,
-      pos: coords.rects.left,
-      translation: coords.translateX,
-      parent: containerDims.width // parseInt(getViewportWidth())
-    },
-    {
-      length: coords.rects.height,
-      pos: coords.rects.top,
-      translation: coords.translateY,
-      parent: containerDims.height // parseInt(getViewportHeight())
-    }
-  ]
-
-  for (var i = 0; i < array.length; i++) {
-    var temp = process(array[i]);
-    // console.log("tweakIt", temp)
-    array[i].newPos = (typeof(temp) === 'number') ? temp : array[i].translation;
-  }
-
-  const coordsNew = {}
-
-  // Object.assign is es6, but there's a polyfill, in case of anything:
-  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
-  Object.assign(coordsNew, coords)
-
-  coordsNew.translateX = array[0].newPos;
-  coordsNew.translateY = array[1].newPos;
-
-  function process(axis) {
-
-    if (axis.length <= axis.parent) {
-      if (axis.pos > (axis.parent - axis.length)) {
-        return ( axis.translation - (axis.pos - (axis.parent - axis.length)) );
-      } else if (axis.pos < (0)) {
-        return ( axis.translation + Math.abs(axis.pos) + 2 );
-      }
-      return false;
-    } else if (axis.length > axis.parent) {
-      if (axis.pos > (0)) {
-        return ( axis.translation - axis.pos );
-      } else if (axis.pos < (axis.parent - axis.length)) {
-        return ( (axis.translation + ( Math.abs(axis.pos) - Math.abs(axis.parent - axis.length) )) );
-      }
-      return false;
-    }
-    return false;
-  }
-
-  return coordsNew;
-  //this.element.css( 'transform', 'matrix(' + coords.scaleX + ', 0, 0, ' + coords.scaleY +  ', ' + x.newPos + ', ' + y.newPos + ')' );
-
-}
-
 export {ScaleCore}
 
 /*
@@ -104,6 +133,6 @@ const ScaleCore = {
     return ScaleCoreCapsule()
   }
 }
- 
+
 // export {ScaleCoreCapsule}
 */
